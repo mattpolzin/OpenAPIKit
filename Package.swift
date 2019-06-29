c0e36cd0d9de07b21c0c9e5b657320414ba44d99
Mathew Polzin <matt.polzin@gmail.com>
2019-06-29 11:07:48 -0700

    Lock dependencies down to the curret major version

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
             targets: ["OpenAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mattpolzin/Poly.git", from: "2.0.0"),
-        .package(url: "https://github.com/mattpolzin/Sampleable.git", from: "2.0.0"),
-        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.1.0")
+        .package(url: "https://github.com/mattpolzin/Poly.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/mattpolzin/Sampleable.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
