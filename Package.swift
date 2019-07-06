0897ba71370cac5295042fafaec3b6c5c7465b7d
Mathew Polzin <matt.polzin@gmail.com>
2019-07-06 14:56:56 -0700

    back swift tools version off to 5 and set language support to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -26,5 +26,6 @@ let package = Package(
         .testTarget(
             name: "OpenAPITests",
             dependencies: ["OpenAPI"]),
-    ]
+    ],
+    swiftLanguageVersions: [ .v5 ]
 )
