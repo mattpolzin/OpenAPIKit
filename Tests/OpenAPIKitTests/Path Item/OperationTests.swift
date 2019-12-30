df8604527c0404baf876304b09414e872e700ad5
Mathew Polzin <matt.polzin@gmail.com>
2019-12-29 22:04:22 -0800

    Add a couple of missing test stub files

---
+++ Tests/OpenAPIKitTests/Path Item/OperationTests.swift
@@ -0,0 +1,13 @@
+//
+//  OperationTests.swift
+//  
+//
+//  Created by Mathew Polzin on 12/29/19.
+//
+
+import XCTest
+import OpenAPIKit
+
+final class OperationTests: XCTestCase {
+    // TODO: write tests
+}
---
+++ Tests/OpenAPIKitTests/Path Item/ParameterTests.swift
@@ -0,0 +1,13 @@
+//
+//  ParameterTests.swift
+//  
+//
+//  Created by Mathew Polzin on 12/29/19.
+//
+
+import XCTest
+import OpenAPIKit
+
+final class ParameterTests: XCTestCase {
+    // TODO: write tests
+}
