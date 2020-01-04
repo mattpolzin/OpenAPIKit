162bc44e585c826b2b9c3d7dcc827a7787775215
Mathew Polzin <matt.polzin@gmail.com>
2020-01-03 22:17:22 -0800

    stub out security scheme tests

---
+++ Tests/OpenAPIKitTests/SecuritySchemeTests.swift
@@ -0,0 +1,13 @@
+//
+//  SecuritySchemeTests.swift
+//  
+//
+//  Created by Mathew Polzin on 1/3/20.
+//
+
+import XCTest
+import OpenAPIKit
+
+final class SecuritySchemeTests: XCTestCase {
+    // TODO: write tests
+}
