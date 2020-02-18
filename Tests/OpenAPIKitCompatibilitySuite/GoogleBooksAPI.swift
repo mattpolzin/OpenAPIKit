5cbde670dc4db0610a3c56179cc263aa42a61990
Mathew Polzin <matt.polzin@gmail.com>
2020-02-17 18:47:12 -0800

    Add a few more tests in the Google Books API compat suite.

--- Tests/OpenAPIKitCompatibilitySuite/GoogleBooksAPI.swift
+++ Tests/OpenAPIKitCompatibilitySuite/GoogleBooksAPI.swift
@@ -44,7 +44,7 @@ final class GoogleBooksAPICampatibilityTests: XCTestCase {
         }
     }
 
-    func test_successfulParse() {
+    func test_successfullyParsedDocument() {
         switch booksAPI {
         case nil:
             XCTFail("Did not attempt to pull Google Books API documentation like expected.")
@@ -73,8 +73,8 @@ final class GoogleBooksAPICampatibilityTests: XCTestCase {
     func test_successfullyParsedBasicMetadata() {
         guard let apiDoc = apiDoc else { return }
 
-        // title is set
-        XCTAssertFalse(apiDoc.info.title.isEmpty)
+        // title is Books
+        XCTAssertEqual(apiDoc.info.title, "Books")
 
         // description is set
         XCTAssertFalse(apiDoc.info.description?.isEmpty ?? true)
@@ -83,9 +83,47 @@ final class GoogleBooksAPICampatibilityTests: XCTestCase {
         XCTAssertEqual(apiDoc.info.contact?.name, "Google")
 
         // no contact email is provided
-        XCTAssertTrue(apiDoc.info.contact?.email?.isEmpty ?? true)
+        XCTAssert(apiDoc.info.contact?.email?.isEmpty ?? true)
 
         // there is at least one tag defined
         XCTAssertFalse(apiDoc.tags?.isEmpty ?? true)
+
+        // server is specified
+        XCTAssertNotNil(apiDoc.servers.first)
+    }
+
+    func test_successfullyParsedRoutes() {
+        guard let apiDoc = apiDoc else { return }
+
+        // just check for a few of the known paths
+        XCTAssert(apiDoc.paths.contains(key: "/cloudloading/addBook"))
+        XCTAssert(apiDoc.paths.contains(key: "/cloudloading/deleteBook"))
+        XCTAssert(apiDoc.paths.contains(key: "/cloudloading/updateBook"))
+        XCTAssert(apiDoc.paths.contains(key: "/dictionary/listOfflineMetadata"))
+        XCTAssert(apiDoc.paths.contains(key: "/familysharing/getFamilyInfo"))
+        XCTAssert(apiDoc.paths.contains(key: "/familysharing/share"))
+
+        // check for a known POST response
+        XCTAssertNotNil(apiDoc.paths["/cloudloading/addBook"]?.post?.responses[200 as OpenAPI.Response.StatusCode])
+
+        // and a known GET response
+        XCTAssertNotNil(apiDoc.paths["/dictionary/listOfflineMetadata"]?.get?.responses[200 as OpenAPI.Response.StatusCode])
+
+        // check for parameters
+        XCTAssertFalse(apiDoc.paths["/dictionary/listOfflineMetadata"]?.parameters.isEmpty ?? true)
+    }
+
+    func test_successfullyParsedComponents() {
+        guard let apiDoc = apiDoc else { return }
+
+        // check for a known parameter
+        XCTAssertNotNil(apiDoc.components.parameters["alt"])
+        XCTAssertTrue(apiDoc.components.parameters["alt"]?.parameterLocation.inQuery ?? false)
+
+        // check for known schema
+        XCTAssertNotNil(apiDoc.components.schemas["Annotation"])
+
+        // check for oauth flow
+        XCTAssertNotNil(apiDoc.components.securitySchemes["Oauth2"])
     }
 }
