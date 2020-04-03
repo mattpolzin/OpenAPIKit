0f4c62cabdaa374da2dcac281eef55efff7cd4fb
Mathew Polzin <matt.polzin@gmail.com>
2020-04-02 21:53:41 -0700

    throw in a little additional code documentation.

--- Sources/OpenAPIKit/Path Item/PathItem.swift
+++ Sources/OpenAPIKit/Path Item/PathItem.swift
@@ -84,39 +84,47 @@ extension OpenAPI {
             self.trace = trace
         }
 
+        public typealias Map = OrderedDictionary<Path, PathItem>
+
+        /// Set the `GET` endpoint operation.
         public mutating func get(_ op: Operation?) {
             get = op
         }
 
+        /// Set the `PUT` endpoint operation.
         public mutating func put(_ op: Operation?) {
             put = op
         }
 
+        /// Set the `POST` endpoint operation.
         public mutating func post(_ op: Operation?) {
             post = op
         }
 
+        /// Set the `DELETE` endpoint operation.
         public mutating func delete(_ op: Operation?) {
             delete = op
         }
 
+        /// Set the `OPTIONS` endpoint operation.
         public mutating func options(_ op: Operation?) {
             options = op
         }
 
+        /// Set the `HEAD` endpoint operation.
         public mutating func head(_ op: Operation?) {
             head = op
         }
 
+        /// Set the `PATCH` endpoint operation.
         public mutating func patch(_ op: Operation?) {
             patch = op
         }
 
+        /// Set the `TRACE` endpoint operation.
         public mutating func trace(_ op: Operation?) {
             trace = op
         }
-
-        public typealias Map = OrderedDictionary<Path, PathItem>
     }
 }
 
