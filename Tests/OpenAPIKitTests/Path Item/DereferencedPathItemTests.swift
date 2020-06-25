6cceabdff372992d93230fa2a0ae257701822843
Mathew Polzin <matt.polzin@gmail.com>
2020-06-24 18:32:00 -0700

    stub out new test file.

---
+++ Tests/OpenAPIKitTests/Path Item/DereferencedPathItemTests.swift
@@ -0,0 +1,13 @@
+//
+//  DereferencedPathItemTests.swift
+//  
+//
+//  Created by Mathew Polzin on 6/23/20.
+//
+
+import XCTest
+import OpenAPIKit
+
+final class DereferencedPathItemTests: XCTestCase {
+    // TODO
+}
