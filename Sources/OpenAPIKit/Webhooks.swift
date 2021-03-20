2fafe00a4063119adf0426d2e8f0b8532cce700a
Mihaela MJ <mihaelamj@me.com>
2021-03-20 16:40:15 +0100

    Added namespaces for `webhooks`

--- Sources/OpenAPIKit/Webhooks.swift
+++ Sources/OpenAPIKit/Webhooks.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  Webhooks.swift
 //  
 //
 //  Created by Mihaela Mihaljevic Jakic on 20.03.2021..
 //
 
+import OpenAPIKitCore
 import Foundation
+
+extension OpenAPI {
+    public typealias Webhooks = OrderedDictionary<String, PathItem>
+    public typealias WebhooksMap = OrderedDictionary<String, Either<JSONReference<Webhooks>, Webhooks>>
+}
