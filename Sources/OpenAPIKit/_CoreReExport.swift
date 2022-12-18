2cfe6e24161a41c214fb8c408b128067c22cb140
Mathew Polzin <matt.polzin@gmail.com>
2022-12-17 23:18:45 -0600

    Alias stuff into desired namespaces in re-export file.

--- Sources/OpenAPIKit/_CoreReExport.swift
+++ Sources/OpenAPIKit/_CoreReExport.swift
@@ -8,7 +8,33 @@
 @_exported import struct OpenAPIKitCore.AnyCodable
 @_exported import struct OpenAPIKitCore.CodingPathError
 @_exported import enum OpenAPIKitCore.Either
-@_exported import enum OpenAPIKitCore.OpenAPI
 @_exported import protocol OpenAPIKitCore.OpenAPIError
 @_exported import struct OpenAPIKitCore.OrderedDictionary
 @_exported import struct OpenAPIKitCore.URLTemplate
+
+import OpenAPIKitCore
+
+public extension OpenAPI {
+    typealias HttpMethod = OpenAPIKitCore.HttpMethod
+    typealias ContentType = OpenAPIKitCore.ContentType
+    typealias Error = OpenAPIKitCore.Error
+    typealias Warning = OpenAPIKitCore.Warning
+    typealias Path = OpenAPIKitCore.Path
+    typealias ComponentKey = OpenAPIKitCore.ComponentKey
+    typealias Discriminator = OpenAPIKitCore.Discriminator
+    typealias ExternalDocumentation = OpenAPIKitCore.ExternalDocumentation
+}
+
+public extension JSONSchema {
+    typealias Permissions = OpenAPIKitCore.JSONSchemaPermissions
+}
+
+public extension JSONTypeFormat {
+    typealias AnyFormat = OpenAPIKitCore.AnyFormat
+    typealias BooleanFormat = OpenAPIKitCore.BooleanFormat
+    typealias ObjectFormat = OpenAPIKitCore.ObjectFormat
+    typealias ArrayFormat = OpenAPIKitCore.ArrayFormat
+    typealias NumberFormat = OpenAPIKitCore.NumberFormat
+    typealias IntegerFormat = OpenAPIKitCore.IntegerFormat
+    typealias StringFormat = OpenAPIKitCore.StringFormat
+}
