a9917966651a35d4204e03fb9358a695b211347e
Mathew Polzin <matt.polzin@gmail.com>
2022-12-24 13:36:27 -0600

    Add some more test coverage for compat layer.

--- Tests/OpenAPIKitCompatTests/DocumentConversionTests.swift
+++ Tests/OpenAPIKitCompatTests/DocumentConversionTests.swift
@@ -11,7 +11,7 @@ import OpenAPIKitCompat
 import XCTest
 
 final class DocumentConversionTests: XCTestCase {
-    func test_barebonesDocument() {
+    func test_barebonesDocument() throws {
         let oldDoc = OpenAPIKit30.OpenAPI.Document(
             openAPIVersion: .v3_0_3,
             info: .init(title: "Hello World", version: "1.0.1"),
@@ -22,11 +22,11 @@ final class DocumentConversionTests: XCTestCase {
 
         let newDoc = oldDoc.convert(to: .v3_1_0)
 
-        assertEqualOldToNew(newDoc, oldDoc)
+        try assertEqualOldToNew(newDoc, oldDoc)
         XCTAssertEqual(newDoc.openAPIVersion, .v3_1_0)
     }
 
-    func test_vendorExtensionsOnDoc() {
+    func test_vendorExtensionsOnDoc() throws {
         let oldDoc = OpenAPIKit30.OpenAPI.Document(
             openAPIVersion: .v3_0_3,
             info: .init(title: "Hello World", version: "1.0.1"),
@@ -38,10 +38,10 @@ final class DocumentConversionTests: XCTestCase {
 
         let newDoc = oldDoc.convert(to: .v3_1_0)
 
-        assertEqualOldToNew(newDoc, oldDoc)
+        try assertEqualOldToNew(newDoc, oldDoc)
     }
 
-    func test_fullInfo() {
+    func test_fullInfo() throws {
         let oldDoc = OpenAPIKit30.OpenAPI.Document(
             info: .init(
                 title: "Hello World",
@@ -59,10 +59,10 @@ final class DocumentConversionTests: XCTestCase {
 
         let newDoc = oldDoc.convert(to: .v3_1_0)
 
-        assertEqualOldToNew(newDoc, oldDoc)
+        try assertEqualOldToNew(newDoc, oldDoc)
     }
 
-    func test_servers() {
+    func test_servers() throws {
         let oldDoc = OpenAPIKit30.OpenAPI.Document(
             info: .init(title: "Hello", version: "1.0.0"),
             servers: [
@@ -85,13 +85,112 @@ final class DocumentConversionTests: XCTestCase {
 
         let newDoc = oldDoc.convert(to: .v3_1_0)
 
-        assertEqualOldToNew(newDoc, oldDoc)
+        try assertEqualOldToNew(newDoc, oldDoc)
+    }
+
+    func test_paths() throws {
+        let params: OpenAPIKit30.OpenAPI.Parameter.Array = [
+            .a(.external(URL(string: "https://welcome.com")!)),
+            .a(.component(named: "test")),
+            .parameter(.init(name: "test", context: .query, schema: .string))
+        ]
+
+        let externalDocs = OpenAPIKit30.OpenAPI.ExternalDocumentation(
+            description: "hello",
+            url: URL(string: "https://website.com")!,
+            vendorExtensions: ["x-hi": 3]
+        )
+
+        let request = OpenAPIKit30.OpenAPI.Request(
+            description: "describble",
+            content: [
+                .json: .init(schema: .string, example: "{\"hi\": 1}", encoding: ["utf8": .init()])
+            ],
+            required: true,
+            vendorExtensions: ["x-tend": "ed"]
+        )
+
+        let response = OpenAPIKit30.OpenAPI.Response(
+            description: "hello",
+            headers: ["Content-Type": .header(.init(schema: .string))],
+            content: [.json: .init(schema: .string)],
+            links: ["link1": .link(operationId: "link1")]
+        )
+
+        let callbacks: OpenAPIKit30.OpenAPI.Callbacks = [
+            .init(url: URL(string: "https://website.com")!): .init(summary: "hello")
+        ]
+
+        let server = OpenAPIKit30.OpenAPI.Server(
+            url: URL(string: "https://website.com")!,
+            description: "ok",
+            variables: ["x-ok": .init(default: "1.0")],
+            vendorExtensions: ["x-cool": 1.5]
+        )
+
+        let securityRequirement: OpenAPIKit30.OpenAPI.SecurityRequirement = [
+            .component(named: "security"): ["hello"]
+        ]
+
+        let operation = OpenAPIKit30.OpenAPI.Operation(
+            tags: ["hello"],
+            summary: "sum",
+            description: "described",
+            externalDocs: externalDocs,
+            operationId: "ident",
+            parameters: params,
+            requestBody: .request(request),
+            responses: [200: .b(response)],
+            callbacks: ["callback": .b(callbacks)],
+            deprecated: true,
+            security: [securityRequirement],
+            servers: [server],
+            vendorExtensions: ["x-hello": 101]
+        )
+
+        let oldDoc = OpenAPIKit30.OpenAPI.Document(
+            info: .init(title: "Hello", version: "1.0.0"),
+            servers: [],
+            paths: [
+                "/hello/world": .init(
+                    summary: "sum",
+                    description: "described",
+                    servers: [
+                        .init(
+                            url: URL(string: "https://website.com")!,
+                            description: "ok",
+                            variables: ["x-ok": .init(default: "1.0")],
+                            vendorExtensions: ["x-cool": 1.5]
+                        )
+                    ],
+                    parameters: [
+                        .a(.external(URL(string: "https://welcome.com")!)),
+                        .a(.internal(.component(name: "test"))),
+                        .parameter(.init(name: "test", context: .query, schema: .string))
+                    ],
+                    get: operation,
+                    put: operation,
+                    post: operation,
+                    delete: operation,
+                    options: operation,
+                    head: operation,
+                    patch: operation,
+                    trace: operation,
+                    vendorExtensions: ["x-test": 123]
+                )
+            ],
+            components: .noComponents
+        )
+
+        let newDoc = oldDoc.convert(to: .v3_1_0)
+
+        try assertEqualOldToNew(newDoc, oldDoc)
     }
 
     // TODO: more tests
 }
 
-fileprivate func assertEqualOldToNew(_ newDoc: OpenAPIKit.OpenAPI.Document, _ oldDoc: OpenAPIKit30.OpenAPI.Document) {
+fileprivate func assertEqualOldToNew(_ newDoc: OpenAPIKit.OpenAPI.Document, _ oldDoc: OpenAPIKit30.OpenAPI.Document) throws {
     // INFO
     XCTAssertEqual(newDoc.info.title, oldDoc.info.title)
     XCTAssertEqual(newDoc.info.version, oldDoc.info.version)
@@ -114,6 +213,11 @@ fileprivate func assertEqualOldToNew(_ newDoc: OpenAPIKit.OpenAPI.Document, _ ol
     }
 
     // PATHS
+    XCTAssertEqual(newDoc.paths.count, oldDoc.paths.count)
+    for (path, newPathItem) in newDoc.paths {
+        let oldPathItem = try XCTUnwrap(oldDoc.paths[path])
+        try assertEqualOldToNew(newPathItem, oldPathItem)
+    }
 
     // COMPONENTS
 
@@ -139,5 +243,68 @@ fileprivate func assertEqualOldToNew(_ newServer: OpenAPIKit.OpenAPI.Server, _ o
         XCTAssertEqual(newVariable.`enum`, oldVariable?.`enum`)
         XCTAssertEqual(newVariable.`default`, oldVariable?.`default`)
         XCTAssertEqual(newVariable.vendorExtensions, oldVariable?.vendorExtensions)
+    }
+}
+
+fileprivate func assertEqualOldToNew(_ newPathItem: OpenAPIKit.OpenAPI.PathItem, _ oldPathItem: OpenAPIKit30.OpenAPI.PathItem) throws {
+    XCTAssertEqual(newPathItem.summary, oldPathItem.summary)
+    XCTAssertEqual(newPathItem.description, oldPathItem.description)
+    if let newServers = newPathItem.servers {
+        let oldServers = try XCTUnwrap(oldPathItem.servers)
+        for (newServer, oldServer) in zip(newServers, oldServers) {
+                assertEqualOldToNew(newServer, oldServer)
+        }
+    }
+    for (newParameter, oldParameter) in zip(newPathItem.parameters, oldPathItem.parameters) {
+        switch (newParameter, oldParameter) {
+        case (.a(let ref), .a(let ref2)):
+            XCTAssertNil(ref.summary)
+            XCTAssertNil(ref.description)
+            XCTAssertEqual(ref.jsonReference.absoluteString, ref2.absoluteString)
+        case (.b(let param), .b(let param2)):
+            XCTAssertEqual(param.name, param2.name)
+            assertEqualOldToNew(param.context, param2.context)
+            XCTAssertEqual(param.description, param2.description)
+            XCTAssertEqual(param.deprecated, param2.deprecated)
+            XCTAssertEqual(param.vendorExtensions, param2.vendorExtensions)
+            XCTAssertEqual(param.required, param2.required)
+        default:
+            XCTFail("Parameters are not equal because one is a reference and the other is not: \(newParameter)  / \(oldParameter)")
+        }
+    }
+    try assertEqualOldToNew(newPathItem.get, oldPathItem.get)
+    try assertEqualOldToNew(newPathItem.put, oldPathItem.put)
+    try assertEqualOldToNew(newPathItem.post, oldPathItem.post)
+    try assertEqualOldToNew(newPathItem.delete, oldPathItem.delete)
+    try assertEqualOldToNew(newPathItem.options, oldPathItem.options)
+    try assertEqualOldToNew(newPathItem.head, oldPathItem.head)
+    try assertEqualOldToNew(newPathItem.patch, oldPathItem.patch)
+    try assertEqualOldToNew(newPathItem.trace, oldPathItem.trace)
+
+    XCTAssertEqual(newPathItem.vendorExtensions, oldPathItem.vendorExtensions)
+}
+
+fileprivate func assertEqualOldToNew(_ newParamContext: OpenAPIKit.OpenAPI.Parameter.Context, _ oldParamContext: OpenAPIKit30.OpenAPI.Parameter.Context) {
+    switch (newParamContext, oldParamContext) {
+    case (.query(required: let req, allowEmptyValue: let empty), .query(required: let req2, allowEmptyValue: let empty2)):
+        XCTAssertEqual(req, req2)
+        XCTAssertEqual(empty, empty2)
+    case (.header(required: let req), .header(required: let req2)):
+        XCTAssertEqual(req, req2)
+    case (.path, .path):
+        break
+    case (.cookie(required: let req), .cookie(required: let req2)):
+        XCTAssertEqual(req, req2)
+    default:
+        XCTFail("Parameter contexts are not equal. \(newParamContext)  /   \(oldParamContext)")
+    }
+}
+
+fileprivate func assertEqualOldToNew(_ newOperation: OpenAPIKit.OpenAPI.Operation?, _ oldOperation: OpenAPIKit30.OpenAPI.Operation?) throws {
+    if let newOp = newOperation {
+        let oldOp = try XCTUnwrap(oldOperation)
+        // TODO
+    } else {
+        XCTAssertNil(oldOperation)
     }
 }
