540d1f6806c979089e418c3227ff6644b057b6cb
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2023-06-16 01:51:15 +0300

    Remove usage of `.documentContainsPaths` from the `OpenAPIKit` documentation.

--- Sources/OpenAPIKit/Validator/Validator.swift
+++ Sources/OpenAPIKit/Validator/Validator.swift
@@ -100,7 +100,6 @@ extension OpenAPI.Document {
 ///
 ///     let document = OpenAPI.Document(...)
 ///     let validator = Validator()
-///         .validating(.documentContainsPaths)
 ///         .validating(.pathsContainOperations)
 ///     try document.validate(using: validator)
 ///
