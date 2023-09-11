a2d5480b6a98d4fed9fa190ff54111f230483cfa
Alberto Lagos <alberto.lagos@gmail.com>
2023-09-11 20:34:34 -0300

    #294: Add `LocallyDereferenceable` conformance for `OpenAPI.CallbackURL` (OpenAPIKit30)

--- Sources/OpenAPIKit30/Callbacks.swift
+++ Sources/OpenAPIKit30/Callbacks.swift
@@ -18,4 +18,20 @@ extension OpenAPI {
 
     /// A map of named collections of Callback Objects (`OpenAPI.Callbacks`).
     public typealias CallbacksMap = OrderedDictionary<String, Either<JSONReference<Callbacks>, Callbacks>>
+
+    /// A dictionary of Dereferenced of callbacks.
+    public typealias DereferencedCallbacks = OrderedDictionary<CallbackURL, DereferencedPathItem>
+
+    /// A dictionary of Dereferenced map of callbacks.
+    public typealias DereferencedCallbacksMap = OrderedDictionary<String, DereferencedCallbacks>
+}
+
+extension OpenAPI.CallbackURL: LocallyDereferenceable {
+    public func _dereferenced(
+        in components: OpenAPI.Components,
+        following references: Set<AnyHashable>,
+        dereferencedFromComponentNamed name: String?
+    ) throws -> OpenAPI.CallbackURL {
+        self
+    }
 }
