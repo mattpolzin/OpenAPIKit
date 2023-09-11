929d5a133c3b348c517219b0d6e5a22a00d32c81
Alberto Lagos <alberto.lagos@gmail.com>
2023-09-11 20:31:43 -0300

    #294: Add `DereferencedCallbacks` & `DereferencedCallbacksMap`

--- Sources/OpenAPIKit/Callbacks.swift
+++ Sources/OpenAPIKit/Callbacks.swift
@@ -19,4 +19,11 @@ extension OpenAPI {
 
     /// A map of named collections of Callback Objects (`OpenAPI.Callbacks`).
     public typealias CallbacksMap = OrderedDictionary<String, Either<OpenAPI.Reference<Callbacks>, Callbacks>>
+
+    /// A dictionary of Dereferenced of callbacks.
+    public typealias DereferencedCallbacks = OrderedDictionary<CallbackURL, DereferencedPathItem>
+
+    /// A dictionary of Dereferenced map of callbacks.
+    public typealias DereferencedCallbacksMap = OrderedDictionary<String, DereferencedCallbacks>
+}
 }
