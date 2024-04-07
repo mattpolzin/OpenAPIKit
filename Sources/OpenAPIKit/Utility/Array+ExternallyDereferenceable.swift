d2aea20abdb5236e57ce0502899956481852f682
Mathew Polzin <matt.polzin@gmail.com>
2024-04-06 19:25:02 -0500

    Add some external dereferencing convenience around array and optional

---
+++ Sources/OpenAPIKit/Utility/Array+ExternallyDereferenceable.swift
@@ -0,0 +1,27 @@
+//
+//  Array+ExternallyDereferenceable.swift
+//
+
+import OpenAPIKitCore
+
+extension Array where Element: ExternallyDereferenceable {
+
+    public func externallyDereferenced<Context: ExternalLoaderContext>(with loader: Context.Type) async throws -> (Self, OpenAPI.Components) {
+        try await withThrowingTaskGroup(of: (Element, OpenAPI.Components).self) { group in
+            for elem in self {
+                group.addTask {
+                    return try await elem.externallyDereferenced(with: loader)
+                }
+            }
+
+            var newElems = Self()
+            var newComponents = OpenAPI.Components()
+
+            for try await (elem, components) in group {
+                newElems.append(elem)
+                try newComponents.merge(components)
+            }
+            return (newElems, newComponents)
+        }
+    }
+}
