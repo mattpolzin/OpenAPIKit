da4e248f25a4cb216cf82f765b0fba7642c7d9e8
Mathew Polzin <matt.polzin@gmail.com>
2026-01-27 10:30:22 -0600

    bump swift requirement to 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -74,5 +74,5 @@ let package = Package(
             name: "OpenAPIKitCompatTests",
             dependencies: ["OpenAPIKitCompat"])
     ],
-    swiftLanguageVersions: [ .v5 ]
+    swiftLanguageModes: [ .v5, .v6 ]
 )
