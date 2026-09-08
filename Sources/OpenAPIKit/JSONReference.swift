1c1bb9dc84f61f29a3026392bd1c27dded6bcf2e
Mathew Polzin <matt.polzin@gmail.com>
2026-09-08 09:23:04 -0500

    give the external loader component key function the full loaded object instad of just its type

--- Sources/OpenAPIKit/JSONReference.swift
+++ Sources/OpenAPIKit/JSONReference.swift
@@ -626,8 +626,8 @@ extension JSONReference: ExternallyDereferenceable where ReferenceType: External
         case .internal(let ref):
             return (.internal(ref), .init(), [])
         case .external(let url):
-            let componentKey = try loader.componentKey(type: ReferenceType.self, at: url)
             let (component, messages): (ReferenceType, [Loader.Message]) = try await loader.load(url)
+            let componentKey = try loader.componentKey(for: component, at: url)
             var components = OpenAPI.Components()
             switch ReferenceType.openAPIComponentsKeyPath {
             case .a(let directPath):
